5166db131e37f6af8737a367df9f88226e6983c2
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-11-09 13:01:17 +0530

    Add files via upload

---
+++ midsemc/q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    double length = 20, area;
+    area = length * length;
+    printf("Area of square the square whose side length is %.2f cm is %.2f squae cm ", length, area);
+    return 0;
+}
